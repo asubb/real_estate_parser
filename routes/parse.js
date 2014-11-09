93b2e0103c17c75d71413a35e7252d8b3a5b4e72
alexey.subbotin <alexey.subbotin@tinypass.com>
2014-11-09 12:05:41 +0400

    Made a head recursion for parsing pages, added a delay between requests to prevent forbidden response

--- routes/parse.js
+++ routes/parse.js
@@ -7,20 +7,52 @@ var models = require("../model/models");
 var parseAvito = function () {
     var source = "Avito";
 
-    var base = "";
-    var parseItem = function (id, url) {
-        models.Apartment.findOne({id: id, source: source}, function (err, a) {
-            if (err) return console.error(err);
-            if (a) return console.log("skipped Avito: " + id);
+    var headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'accept-encoding': 'gzip,deflate,sdch',
+        'accept-language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+        'cache-control': 'no-cache',
+        'cookie': 'u=1stmee8v.lqg4o6.em0occsjdh; dfp_group=81; sessid=b71a201747c8fe3655fcb69853e4d876.1415424228; v=1415517641; __utma=99926606.1087434079.1415211490.1415514483.1415517640.9; __utmb=99926606.10.10.1415517640; __utmc=99926606; __utmz=99926606.1415211490.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)',
+        'pragma': 'no-cache',
+        'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'
+    };
 
-            var link = base + url;
+    var base = "";
+    var parseItems = function (items) {
+        console.log("-->>" + items.length);
+        if (items.length == 0) {
+            return;
+        }
+        var item = items.pop();
+
+        var scheduleNextParse = function (delay) {
+            setTimeout(function () {
+                // start next parsing
+                parseItems(items);
+            }, delay ? delay : Math.random() * 10000 + 3000);
+        }
+
+        models.Apartment.findOne({id: item.id, source: source}, function (err, a) {
+            if (err) return console.error(err);
+            if (a) {
+                console.log("skipped Avito: " + item.id);
+                scheduleNextParse(0);
+                return;
+            }
+
+            var link = base + item.url;
             request({
                 url: link,
-                encoding: 'utf8'
+                encoding: 'utf8',
+                timeout: 60000,
+                gzip: true,
+                jar: true,
+                headers: headers
             }, function (error, response, body) {
-                console.log("Page " + link);
+                console.log("Item page " + link);
                 if (!error && response.statusCode == 200) {
                     jsdom.env(body, ["http://code.jquery.com/jquery.js"], function (errors, window) {
+                        if (errors) return console.error("parse item jsdom error", errors);
                         var $ = window.$;
                         var p = $("h1[itemprop=name]")[0].innerHTML.match(/(\d+)[^,]+,\s(\d+)[^,]+,\s(\d+\/\d+)/);
                         var rooms = p[1];
@@ -32,8 +64,8 @@ var parseAvito = function () {
                         var district = $(".description_content_company[itemprop=address] span")[0].innerHTML.match(/[^,]+/)[0].split(" ")[1];
                         var description = $("#desc_text")[0].innerHTML;
                         var a = new models.Apartment({
-                            id: id,
-                            url: url,
+                            id: item.id,
+                            url: link,
                             source: source,
                             description: description,
                             district: district,
@@ -45,10 +77,11 @@ var parseAvito = function () {
                         });
 
                         a.save();
-                        console.log('Saved Avito: ' + id);
+                        console.log('Saved Avito: ' + item.id);
+                        scheduleNextParse();
                     });
                 } else {
-                    console.error(error);
+                    console.error(link, error, response ? response.statusCode : "");
                 }
             });
         });
@@ -57,18 +90,25 @@ var parseAvito = function () {
     var parsePage = function (link) {
         request({
             url: link,
-            encoding: 'utf8'
+            encoding: 'utf8',
+            timeout: 60000,
+            gzip: true,
+            jar: true,
+            headers: headers
         }, function (error, response, body) {
             console.log("Page " + link);
             if (!error && response.statusCode == 200) {
                 jsdom.env(body, ["http://code.jquery.com/jquery.js"], function (errors, window) {
+                    if (errors) return console.error("parse page jsdom error", errors, body);
                     var $ = window.$;
 
                     // iterate through items
+                    var items = [];
                     $("div.item").each(function (idx, item) {
                         var url = $(item).find("h3.title a").attr('href');
-                        parseItem(item.id, url);
+                        items.push({id: item.id, url: url});
                     });
+                    parseItems(items);
 
                     // get next link
                     var nextEl = $("div.pagination__nav a.pagination__page");
@@ -81,13 +121,13 @@ var parseAvito = function () {
                         link = link.replace(/file:[\/]+(c:)?/, '');
                         setTimeout(function () {
                             parsePage(base + link);
-                        }, 0);
+                        }, Math.random() * 10000 + 10000);
                     } else {
                         console.log("---- DONE ----")
                     }
                 });
             } else {
-                console.error(error);
+                console.error(link, error, response ? response.statusCode : "");
             }
         });
     };
